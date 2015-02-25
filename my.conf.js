9838884db5e49bc74b62b33bbbd976e120790187
alu0100775846 <alu0100775846@ull.edu.es>
2015-02-25 20:48:57 +0000

    Instalado preprocesador de Karma

--- my.conf.js
+++ my.conf.js
@@ -34,8 +34,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/test.html': ['html2js']
-    },  
+      'tests/index.html': ['html2js']
+    },
 
 
     // test results reporter to use
